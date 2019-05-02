0664073a837d6ee52e4d6f23318054fc18f3c2bc
yudedako <pluto.club@gmail.com>
2019-05-03 07:15:46 +0900

    cssの調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CharacterList, { Character } from "./CharacterList";
+
+import "./App.css";
 
 class App extends Component {
   render() {
-    const logoOptions = {
-      alt: "logo",
-      className: "App-logo",
-      src: logo
-    };
-
-    const title = "こんにちは React";
-    const targets = ["World", "Kanae", "Yukina"];
+    const characters: Character[] = [
+      {
+        id: 1,
+        name: "羽咲　綾乃",
+        age: 16,
+        height: 151
+      },
+      {
+        id: 2,
+        name: "荒垣　なぎさ",
+        age: 18,
+        height: 174
+      },
+      {
+        id: 3,
+        name: "泉　理子",
+        age: 18
+      }
+    ];
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img {...logoOptions}/> {title && <p>{title}</p>}
-          {targets.map(target => (
-            <p>Hello, {target}!</p>
-          ))}
+      <div className="container">
+        <header>
+          <h1>はねバド！ キャラクター一覧</h1>
         </header>
+        <CharacterList school="北小町高校" characters={characters} />
       </div>
     );
   }
